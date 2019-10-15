64c54ba9f37a34d136999b2eb88d7a323cd88fb6
aschambers <aschambers4291@gmail.com>
2019-10-14 22:33:01 -0700

    add another working anagram solution

--- validAnagram.js
+++ validAnagram.js
@@ -4,7 +4,7 @@
  * @param {string} first 
  * @param {string} second 
  */
-const validAnagram = (first, second) => {
+const validAnagramFirst = (first, second) => {
   if (first.length !== second.length) {
     return false;
   }
@@ -15,6 +15,36 @@ const validAnagram = (first, second) => {
   for (let i = 0; i < firstWord.length; i++) {
     if (firstWord[i] !== secondWord[i]) {
       return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(validAnagramFirst('aazab', 'azbaa'));
+
+/**
+ * determine if two words are anagrams,
+ * two words that have same characters rearranged
+ * @param {string} first 
+ * @param {string} second 
+ */
+const validAnagram = (first, second) => {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    lookup[first[i]] ? lookup[first[i]] += 1 : lookup[first[i]] = 1;
+  }
+
+  for (let j = 0; j < second.length; j++) {
+    if (!lookup[second[j]]) {
+      return false;
+    } else {
+      lookup[second[j]] -= 1;
     }
   }
 
