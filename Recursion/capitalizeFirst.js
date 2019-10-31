169b31d402427d0d659a1cb20e80d64f07cd825e
aschambers <aschambers4291@gmail.com>
2019-10-30 19:29:42 -0700

    add capitalizeFirst algorithm

---
+++ Recursion/capitalizeFirst.js
@@ -0,0 +1,16 @@
+/**
+ * recursive function which will
+ * capitalize the first letter of
+ * every word in the array
+ * @param {array} arr
+ */
+const capitalizeFirst = (array) => {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length-1)[0].toUpperCase());
+  return res;
+}
+
+console.log(capitalizeFirst(['car','taco','banana']));
