9b82d4f771549d3116d1dc221bd062461c57c925
aschambers <aschambers4291@gmail.com>
2019-11-08 19:39:30 -0800

    optimize bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -4,16 +4,19 @@
  * array of numbers
  */
 const bubbleSort = (arr) => {
- for (let i = 0; i < arr.length; i++) {
-   for (let j = 0; j < arr.length; j++) {
-     if (arr[j] > arr[j + 1]) {
-       let temp = arr[j];
-       arr[j] = arr[j + 1];
-       arr[j + 1] = temp;
-     }
-   }
- }
- return arr;
+  for (let i = 0; i < arr.length; i++) {
+    let noSwaps = true;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
 }
 
 console.log(bubbleSort([2,8,4,14,11,5,7,22,23,6]));
