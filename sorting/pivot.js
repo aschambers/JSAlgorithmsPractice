faf79c19d2bc7e3494753be8a4cb7c290cd30c29
aschambers <aschambers4291@gmail.com>
2019-11-11 22:06:36 -0800

    work on helper method for moving all elements lower than starting point to the left side called pivot

---
+++ sorting/pivot.js
@@ -0,0 +1,39 @@
+/**
+ * Write a helper function called pivot
+ * which takes a starting index and puts
+ * all elements lower then the starting
+ * index value, on the left side
+ * @param {arr} arr 
+ * @param {number} start 
+ */
+const pivot = (arr, start) => {
+  const pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      // swap element at the pivot index
+      // with the current element
+      swap(arr, swapIndex, i);
+    }
+  }
+  // swap starting element with pivot index
+  swap(arr, start, swapIndex);
+  return arr;
+}
+
+/**
+ * helper function used to swap 
+ * elements in an array
+ * @param {arr} arr 
+ * @param {number} i 
+ * @param {number} j 
+ */
+const swap = (arr, i, j) => {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+console.log(pivot([4,8,3,2,1,6,7,5], 0));
