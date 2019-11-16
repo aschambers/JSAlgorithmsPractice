c9a62a19af5e978cd32390c79eaccc069973f94a
aschambers <aschambers4291@gmail.com>
2019-11-15 21:47:56 -0800

    add prime number to improve hashing function

--- HashTables/firstStringHash.js
+++ HashTables/firstStringHash.js
@@ -3,14 +3,18 @@
  * hash a string only. This is
  * not constant time, scales along
  * with the size of the input.
+ * Use a prime number to improve
+ * hashing function
  * @param {string} key 
  * @param {number} arrayLen 
  */
 const stringHash = (key, arrayLen) => {
   let total = 0;
-  for (let char of key) {
+  let primeNumber = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+    total = (total * primeNumber + value) % arrayLen;
   }
   return total;
 }
