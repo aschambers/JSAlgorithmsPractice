b739c2b9dc41ade3e02214d4b13b305d48195b9d
Allison Schambers <aschambers4291@gmail.com>
2021-03-20 13:47:12 -0700

    Add code to update a node with value provided at a position.

--- LinkedLists/TestingTwo.js
+++ LinkedLists/TestingTwo.js
@@ -168,4 +168,28 @@ const deleteAt = (list, position) => {
   return list;
 }
 
-console.log(deleteAt(deleteList, 1));
+const updateNodeAt = (list, position, value) => {
+  if (size(list) < 1 || !value) return list;
+  if (position > size(list)) return list;
+  if (position === 0) {
+    list.value = value;
+  }
+
+  let previousNode = list;
+  let currentNode = list.next;
+
+  for (let i = 1; i < size(list); i++) {
+    if (position === i) {
+      previousNode.next.value = value;
+      break;
+    }
+
+    previousNode = currentNode;
+    currentNode = previousNode.next;
+  }
+
+  return list;
+}
+
+const updateList = updateNodeAt(deleteList, 2, 25);
+console.log(updateList);
